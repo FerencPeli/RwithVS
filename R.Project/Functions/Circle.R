2518ce471daf1f2006ac07accd7748349c18c625
FerencPeli <ferenc.peli@outlook.com>
2018-06-08 19:33:46 +0200

    Getting rid of bad habits :)

--- R.Project/Functions/Circle.R
+++ R.Project/Functions/Circle.R
@@ -1,7 +1,7 @@
 calculateCircumference <- function(radius = 1) {
-    return(2 * radius * pi);
+    return(2 * radius * pi)
 }
 
 calculateArea <- function(radius = 1) {
-    return(`^`(radius, 2) * pi);
+    return(`^`(radius, 2) * pi)
 }
--- R.Project/Script.R
+++ R.Project/Script.R
@@ -1,13 +1,13 @@
 # import helper file
 source("Functions/Circle.R")
 
-cat("Start of script");
+cat("Start of script")
 
 # calculations
-circleRadius <- 10;
-circleArea <- calculateArea(circleRadius);
-circleCircumference <- calculateCircumference(circleRadius);
+circleRadius <- 10
+circleArea <- calculateArea(circleRadius)
+circleCircumference <- calculateCircumference(circleRadius)
 
 # print results
-cat("\n The area of the circle is", circleArea);
-cat("\n The circumference of the circle is", circleCircumference);
+cat("\n The area of the circle is", circleArea)
+cat("\n The circumference of the circle is", circleCircumference)
